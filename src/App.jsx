f9701d8ff27c4bf5e40c57d2edc6cca5850cfb34
Igwe Uchenna <igweuche42@gmail.com>
2025-09-21 22:03:19 +0100

    creating the todo inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 
 
 function App() {
+// Step 1: Create state for Todos
+const [todos, setTodos] = useState([]);
+const [input, setInput] = useState("");
+
+
+// step 2: Function to add todo
+const addTodo = () => {
+  if (input.trim() === "") return; // ignore empty input
+  setTodos([...todos, input]); // add new todo
+  setInput(""); // clear input
+}
+
+return (
+  <div style={{padding: "20px"}}>
+<h1>Todo App</h1>
+
+        {/* Input field */}
+        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder='Add Task'/>
+  </div>
+)
+
 
 }
 
